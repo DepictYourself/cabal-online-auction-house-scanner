fc25cf73457877fee82616296bc7249111f97f23
DepictYourself <totha014@gmail.com>
2021-10-03 10:34:19 +0200

    Implement basic parser

--- helpers.py
+++ helpers.py
@@ -1,5 +1,56 @@
+import cv2
+import pytesseract
+import numpy
+import pyautogui
 import win32gui
 import win32con
+
+
+
+def make_screenshot():
+    img = pyautogui.screenshot(region=(0, 0, 826, 756))
+    converted_img = convert_to_cv2_img(img)
+    return converted_img
+
+
+def convert_to_cv2_img(img):
+    open_cv_img = numpy.array(img)
+    open_cv_img = cv2.cvtColor(open_cv_img, cv2.COLOR_RGB2BGR)
+    return open_cv_img
+
+
+
+def parse_item_name(img):
+    #                 x1  y1
+    # balfelso_pont= (175,195)
+    #                 x2  y2
+    # jobbalso_pont= (425,235)
+    # numpy slicing: img[y1:y2, x1:x2]
+    img_crop_item = img[195:235, 175:425].copy()
+    # resize to double for more readability
+    img_crop_item = cv2.resize(img_crop_item, (0, 0), fx=2, fy=2)
+    # conver to greyscale
+    img_crop_item = cv2.cvtColor(img_crop_item, cv2.COLOR_BGR2GRAY)
+    # apply simple treshold for easier recognition with pytesseract
+    thr_item = cv2.threshold(img_crop_item, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    item_name = pytesseract.image_to_string(thr_item, config="-c page_separator='' ")
+    return item_name
+
+
+
+def parse_item_quantity(img):
+    img_crop_quantity = img[195:235, 430:465].copy()
+    img_crop_quantity = cv2.resize(img_crop_quantity, (0, 0), fx=2, fy=2)
+    img_crop_quantity = cv2.cvtColor(img_crop_quantity, cv2.COLOR_BGR2GRAY)
+    thr_quantity = cv2.threshold(img_crop_quantity, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    item_quantity = pytesseract.image_to_string(thr_quantity, config="-c page_separator=''")
+    return item_quantity
+
+def parse_item_price(img):
+    img_crop_price = img[195:235, 475:585].copy()
+    img_crop_price = cv2.resize(img_crop_price, (0, 0), fx=2, fy=2)
+    item_price = pytesseract.image_to_string(img_crop_price, config="-c page_separator='' ")
+    return item_price
 
 
 
--- run.py
+++ run.py
@@ -4,7 +4,13 @@ import constants as c
 
 #Run the whole project with "python run.py"
 def run():
+    pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
     initialize_cabal_window()
+    img = make_screenshot()
+    item_name = parse_item_name(img)
+    item_quantity = parse_item_quantity(img)
+    item_price = parse_item_price(img)
+    print(item_name, item_quantity ,item_price)
 
 
 
